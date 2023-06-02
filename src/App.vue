ace78e6e91c6229affda48a42cf6dbfe3f4e558e
Rob Marriott <rob.marriott@sport80.com>
2023-06-02 21:14:05 +0100

    Board cards generated from data. Player totals calculated dynamically

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,108 @@
 <script setup lang="ts">
-// import { reactive } from 'vue'
+import { reactive, computed } from 'vue'
 // import { RouterLink, RouterView } from 'vue-router'
+// import defaultCards from './data/default-cards.js'
 import BoardCard from './components/BoardCard.vue'
+
+// const deck = ref(defaultCards)
+const opponentCards: any[][] = [
+  [
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 2 },
+    { value: 3 }
+  ],
+  [{ value: 6 }, { value: 5 }],
+  [{ value: 8 }]
+]
+const playerCards: any[][] = [
+  [{ value: 4 }, { value: 4 }, { value: 6 }, { value: 6 }, { value: 7 }],
+  [{ value: 5 }, { value: 5 }, { value: 5 }],
+  [{ value: 6 }, { value: 6 }, { value: 8 }, { value: 8 }]
+]
+
+let opponentBoardCards = reactive(opponentCards)
+let playerBoardCards = reactive(playerCards)
+
+const opponentTotal = computed(() => {
+  let total = 0
+  for (let i = 0; i < opponentBoardCards.length; i++) {
+    for (let j = 0; j < opponentBoardCards[i].length; j++) {
+      total = total + opponentBoardCards[i][j].value
+    }
+  }
+  return total
+})
+
+const playerTotal = computed(() => {
+  let total = 0
+  for (let i = 0; i < playerBoardCards.length; i++) {
+    for (let j = 0; j < playerBoardCards[i].length; j++) {
+      total = total + playerBoardCards[i][j].value
+    }
+  }
+  return total
+})
+
+function getOpponentRowTotal(rowIndex: number): number {
+  let total = 0
+  for (let i = 0; i < opponentBoardCards[rowIndex].length; i++) {
+    total = total + opponentBoardCards[rowIndex][i].value
+  }
+  return total
+}
+
+function getPlayerRowTotal(rowIndex: number): number {
+  let total = 0
+  for (let i = 0; i < playerBoardCards[rowIndex].length; i++) {
+    total = total + playerBoardCards[rowIndex][i].value
+  }
+  return total
+}
 </script>
 
 <template>
   <div class="game-container">
     <div class="scroll-container">
       <div class="opponent-board">
-        <div class="card-row">
+        <div v-for="(row, i) in opponentBoardCards" class="card-row" :key="`opponent-row-${i}`">
           <div class="row-stats">
-            <div class="total-counter">999</div>
+            <div class="total-counter">{{ getOpponentRowTotal(i) }}</div>
           </div>
 
           <div class="card-container">
-            <BoardCard v-for="i in 12" :value="i" />
-          </div>
-        </div>
-        <div class="card-row">
-          <div class="row-stats">
-            <div class="total-counter">999</div>
-          </div>
-
-          <div class="card-container">
-            <BoardCard v-for="i in 2" :value="i" />
-          </div>
-        </div>
-        <div class="card-row">
-          <div class="row-stats">
-            <div class="total-counter">999</div>
-          </div>
-
-          <div class="card-container">
-            <BoardCard v-for="i in 3" :value="i" />
+            <BoardCard v-for="card in row" :value="card.value" />
           </div>
         </div>
       </div>
 
       <div class="game-stats">
         <div class="player-stats">
-          <div class="total-counter player-total">999</div>
+          <div class="total-counter player-total">{{ playerTotal }}</div>
         </div>
         <!-- TODO: Display last card in specialCards array, ELSE display placeholder -->
         <BoardCard />
         <div class="opponent-stats">
-          <div class="total-counter opponent-total">76</div>
+          <div class="total-counter opponent-total">{{ opponentTotal }}</div>
         </div>
       </div>
 
       <div class="player-board">
-        <div class="card-row">
+        <div v-for="(row, i) in playerBoardCards" class="card-row">
           <div class="row-stats">
-            <div class="total-counter">999</div>
+            <div class="total-counter">{{ getPlayerRowTotal(i) }}</div>
           </div>
 
           <div class="card-container">
-            <BoardCard v-for="i in 15" :value="i" />
-          </div>
-        </div>
-        <div class="card-row">
-          <div class="row-stats">
-            <div class="total-counter">999</div>
-          </div>
-
-          <div class="card-container">
-            <BoardCard v-for="i in 1" :value="i" />
-          </div>
-        </div>
-        <div class="card-row">
-          <div class="row-stats">
-            <div class="total-counter">999</div>
-          </div>
-
-          <div class="card-container">
-            <BoardCard v-for="i in 3" :value="i" />
+            <BoardCard v-for="card in row" :value="card.value" />
           </div>
         </div>
 
---
+++ src/data/default-cards.js
@@ -0,0 +1,2 @@
+var defaultCards = [{ value: 1 }, { value: 2 }, { value: 4 }, { value: 7 }, { value: 8 }]
+export default defaultCards
